33359c132685a3cb4ea8e954572c8bc2a792bfee
nsobczak <nicolas.sobczak@orange.fr>
2018-09-17 17:33:05 +0200

    added pickup file
    fixed error text not between "

---
+++ BatteryCollector/Source/BatteryCollector/Pickup.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API APickup : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** return the mesh for the pickupk*/
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	/** Static mesh to represent the pickup in the level*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* PickupMesh;
+
+
+};
